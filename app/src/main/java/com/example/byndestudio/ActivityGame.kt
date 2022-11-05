1f803fc68ae09a93cec7347022c419444e1fb1e9
bynex <bynexe@gmail.com>
2022-11-05 17:13:57 +0600

    Updated UI and functions

--- app/src/main/java/com/example/byndestudio/ActivityGame.kt
+++ app/src/main/java/com/example/byndestudio/ActivityGame.kt
@@ -13,18 +13,5 @@ class ActivityGame : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bClass = ActivityGameBinding.inflate(layoutInflater)
         setContentView(bClass.root)
-        bClass..setOnClickListener {
-            showDialog()
-        }
     }
-
-    private fun showDialog() {
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("Новое событие!")
-        builder.setMessage("Что-то явно произошло")
-        builder.setPositiveButton("ОК") {dialog, i -> }
-        builder.show()
-    }
-
-
 }
