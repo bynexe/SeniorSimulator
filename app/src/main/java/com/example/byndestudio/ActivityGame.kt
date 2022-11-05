abaa20d02bd79d0a3d2238c262dbdc23962dea66
bynex <bynexe@gmail.com>
2022-11-05 22:42:41 +0600

    New UI

--- app/src/main/java/com/example/byndestudio/ActivityGame.kt
+++ app/src/main/java/com/example/byndestudio/ActivityGame.kt
@@ -2,8 +2,10 @@ package com.example.byndestudio
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.byndestudio.databinding.ActivityGameBinding
 
 lateinit var bClass : ActivityGameBinding
@@ -15,6 +17,25 @@ class ActivityGame : AppCompatActivity() {
         setContentView(bClass.root)
 
         bClass.job1.setOnClickListener() {
+            bClass.buyWorkers.visibility = View.VISIBLE
+        }
+        bClass.job2.setOnClickListener() {
+            bClass.buyWorkers.visibility = View.VISIBLE
+        }
+        bClass.job3.setOnClickListener() {
+            bClass.buyWorkers.visibility = View.VISIBLE
+        }
+        bClass.job4.setOnClickListener() {
+            bClass.buyWorkers.visibility = View.VISIBLE
+        }
+        bClass.job5.setOnClickListener() {
+            bClass.buyWorkers.visibility = View.VISIBLE
+        }
+        bClass.job6.setOnClickListener() {
+            bClass.buyWorkers.visibility = View.VISIBLE
+        }
+        bClass.exitBuyMenu.setOnClickListener() {
+            bClass.buyWorkers.visibility = View.INVISIBLE
         }
     }
 }
